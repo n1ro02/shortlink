67f1fe3e8c7cc47d06542f30a08b0260c3f006d2
lucen <whr_zm@163.com>
2025-12-29 23:52:27 +0800

    optimize: 优化实体抽象表达

--- admin/src/main/java/com/openai/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/openai/shortlink/admin/dao/entity/UserDO.java
@@ -1,18 +1,17 @@
 package com.openai.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.openai.shortlink.admin.common.database.BaseDO;
 import lombok.Data;
-
-import java.util.Date;
+import lombok.EqualsAndHashCode;
 
 /**
  * 用户持久层实体
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 @TableName("t_user")
-public class UserDO{
+public class UserDO extends BaseDO {
 
 
     /**
@@ -49,23 +48,5 @@ public class UserDO{
      * 注销时间戳
      */
     private Long deletionTime;
-
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Integer delFlag;
 
 }
